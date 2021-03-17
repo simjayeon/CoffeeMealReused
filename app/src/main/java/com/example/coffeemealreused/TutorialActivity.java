dc57fcb479d909c4c231fd71755075f3bc5e51ae
simja <simjayeon98@gmail.com>
2021-03-18 02:09:26 +0900

    Tutorial page and Animation

---
+++ app/src/main/java/com/example/coffeemealreused/TutorialActivity.java
@@ -0,0 +1,176 @@
+package com.example.coffeemealreused;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class TutorialActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private int[] layouts;
+    private Button btnSkip, btnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(Build.VERSION.SDK_INT >= 21){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
+        setContentView(R.layout.activity_tutorial);
+
+        viewPager = findViewById(R.id.view_pager);
+        dotsLayout = findViewById(R.id.layoutDots);
+        btnNext = findViewById(R.id.btn_next);
+        btnSkip = findViewById(R.id.btn_skip);
+
+        layouts = new int[]{
+                R.layout.page1,
+                R.layout.page2,
+                R.layout.page3
+        };
+
+        addBottomDots(0);
+        changeStatusBarColor();
+        pagerAdapter = new PagerAdapter();
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+
+        btnSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveLoginSelectPage();
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int current = getItem(+1);
+                if(current < layouts.length){
+                    viewPager.setCurrentItem(current);
+                }else{
+                    moveMainPage();
+                }
+            }
+        });
+    }
+
+    private void addBottomDots(int currentPage){
+        dots = new TextView[layouts.length];
+
+        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
+        int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
+
+        dotsLayout.removeAllViews();
+        for (int i=0; i<dots.length; i++){
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(colorsInactive[currentPage]);
+            dotsLayout.addView(dots[i]);
+        }
+
+        if(dots.length > 0){
+            dots[currentPage].setTextColor(colorsActive[currentPage]);
+        }
+    }
+
+    private int getItem(int i){
+        return viewPager.getCurrentItem()+i;
+    }
+
+    private void moveMainPage(){
+        startActivity(new Intent(TutorialActivity.this, MainActivity.class));
+        finish();
+    }
+
+    private void moveLoginSelectPage(){
+        startActivity(new Intent(TutorialActivity.this, LoginSelectActivity.class));
+        finish();
+    }
+
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addBottomDots(position);
+
+            if(position == layouts.length-1){
+                btnNext.setText(getString(R.string.start));
+                btnSkip.setVisibility(View.GONE);
+            }else{
+                btnNext.setText(getString(R.string.next));
+                btnSkip.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    private void changeStatusBarColor(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    public class PagerAdapter extends androidx.viewpager.widget.PagerAdapter{
+        private LayoutInflater layoutInflater;
+
+        public PagerAdapter(){}
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position){
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = layoutInflater.inflate(layouts[position], container, false);
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return layouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return view == object;
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
+}
