fdf1a26e74506071a6ed77dc6104a1b59ce07bd0
simja <simjayeon98@gmail.com>
2021-03-20 00:38:02 +0900

    modify the Button Animation

--- app/src/main/java/com/example/coffeemealreused/LoginSelectActivity.java
+++ app/src/main/java/com/example/coffeemealreused/LoginSelectActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.BounceInterpolator;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -15,8 +16,7 @@ public class LoginSelectActivity extends AppCompatActivity {
 
     private Button btnEmail, btnSns, loginKakao, loginGoogle;
     private ConstraintLayout snsLayout;
-    //private Animation translateUp;
-    private ObjectAnimator translateUp;
+    private Animation translateUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +28,16 @@ public class LoginSelectActivity extends AppCompatActivity {
         loginKakao = findViewById(R.id.loginKakao);
         loginGoogle = findViewById(R.id.loginGoogle);
         snsLayout = findViewById(R.id.snsLayout);
-        //translateUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_up);
-        //translateUp = Ani
+        translateUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_up);
 
         btnSns.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ObjectAnimator.ofFloat(snsLayout, "translationY", -100f)
+                        .setDuration(300)
+                        .start();
+                snsLayout.startAnimation(translateUp);
+
                 //snsLayout.startAnimation(translateUp);
                 btnEmail.setVisibility(View.GONE);
                 btnSns.setVisibility(View.GONE);
